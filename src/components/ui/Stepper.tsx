54be2a4577b816ad1dc619b177555e4706f8e69a
perkzen <perko.domen@gmail.com>
2023-12-03 00:06:49 +0100

    fix: stepper

--- src/components/ui/Stepper.tsx
+++ src/components/ui/Stepper.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useQueryParams } from '@/hooks/useQueryParams';
 import { classnames } from '@/utils/classnames';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 interface Step {
   name: string;
@@ -16,9 +16,16 @@ interface StepperProps {
 const Stepper = ({ steps }: StepperProps) => {
   const { urlQuery } = useQueryParams();
   const router = useRouter();
+  const pathname = usePathname();
 
   const isCompleted = (step: Step) => {
     return urlQuery.step > steps.findIndex((s) => s.href === step.href);
+  };
+
+  const handleStepClick = (step: Step) => {
+    if (isCompleted(step) && pathname !== step.href) {
+      router.push(step.href);
+    }
   };
 
   return (
@@ -30,7 +37,7 @@ const Stepper = ({ steps }: StepperProps) => {
               'w-min rounded-full border-2 border-black px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none',
               isCompleted(step) ? 'cursor-pointer bg-primary' : 'bg-primary-200',
             )}
-            onClick={isCompleted(step) ? () => router.push(step.href) : undefined}
+            onClick={() => handleStepClick(step)}
           >
             {step.name}
           </div>
