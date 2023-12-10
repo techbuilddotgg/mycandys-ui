a765f7b2df570c98d5eed53f284f7162e5c436aa
perkzen <perko.domen@gmail.com>
2023-12-10 22:07:06 +0100

    fix: build

--- src/app/profile/orders/page.tsx
+++ src/app/profile/orders/page.tsx
@@ -1,5 +1,8 @@
 import UserOrdersList from '@/components/page/profile/orders/UserOrdersList';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const Page = () => {
   return <UserOrdersList />;
 };
