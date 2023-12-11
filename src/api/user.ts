874c52a6b0cbcea59d1c5f1e9e5f8c11021e1ec0
Matevz <semprimoznik.matevz@gmail.com>
2023-12-11 13:18:58 +0100

    feat: update fetch for user info and update

--- src/api/user.ts
+++ src/api/user.ts
@@ -2,15 +2,20 @@ import { AxiosResponse } from 'axios';
 import { APIRoute } from '@/api/routes';
 import { UpdateUser, User } from '@/models/user';
 import { api } from '@/api/axios';
+import { getSession } from '@/utils/session';
 
 export const getUser = async () => {
-  const { data } = (await api.get(`${APIRoute.USERS}`)) as AxiosResponse<User>;
+  const session = getSession();
+  const { data } = (await api.get(
+    `${APIRoute.USERS}/${session?.userId}`,
+  )) as AxiosResponse<User>;
   return data;
 };
 
 export const updateUser = async (body: UpdateUser) => {
+  const session = getSession();
   const { data } = (await api.put(
-    `${APIRoute.USERS}`,
+    `${APIRoute.USERS}/${session?.userId}`,
     body,
   )) as AxiosResponse<UpdateUser>;
 
