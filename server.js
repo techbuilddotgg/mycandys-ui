000013b2028d2a8410ad300bc14c0900ad67fe6c
Matevz <semprimoznik.matevz@gmail.com>
2023-12-11 16:19:05 +0100

    fix: deployment

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const next = require('next');
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+// creating the app either in production or dev mode
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+// running the app, async operation
+app.prepare().then(() => {
+  const server = express();
+
+  // redirecting all requests to Next.js
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Runing on port ${port}, dev: ${dev}`);
+  });
+});
