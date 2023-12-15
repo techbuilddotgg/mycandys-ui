3599dc21660237d247b226938522f61a1b0b3ae7
perkzen <perko.domen@gmail.com>
2023-12-15 14:22:14 +0100

    feat: remove suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,28 @@
-import { ReactNode, Suspense } from 'react';
+import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Public_Sans } from 'next/font/google';
 import Navbar from '@/components/ui/Navbar';
 import './globals.css';
 import Providers from '@/components/providers/Providers';
-import Loading from '@/components/ui/Loading';
 
 const publicSans = Public_Sans({ subsets: ['latin-ext'] });
 
 export const metadata: Metadata = {
-  title: "MyCandy's",
-  description: "MyCandy's the best candy shop in the world",
+  title: 'MyCandy\'s',
+  description: 'MyCandy\'s the best candy shop in the world',
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={publicSans.className}>
-        <Providers>
-          <div className={'flex min-h-screen flex-col bg-secondary'}>
-            <Navbar />
-            <Suspense
-              fallback={
-                <main className="flex min-h-screen flex-col items-center p-24 pb-10">
-                  <Loading />
-                </main>
-              }
-            >
-              {children}
-            </Suspense>
-          </div>
-        </Providers>
-      </body>
+    <body className={publicSans.className}>
+    <Providers>
+      <div className={'flex min-h-screen flex-col bg-secondary'}>
+        <Navbar />
+        {children}
+      </div>
+    </Providers>
+    </body>
     </html>
   );
 }
